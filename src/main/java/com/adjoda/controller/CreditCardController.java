4432fe09a3015e56c99eeacf90658549d63ef74b
Mawuli ADJODA <koffimawulli.adjoda@gmail.com>
2024-06-08 23:35:52 +0200

    improve api test

--- src/main/java/com/adjoda/controller/CreditCardController.java
+++ src/main/java/com/adjoda/controller/CreditCardController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,9 +14,10 @@ public class CreditCardController {
 
     private final CreditCardLimitIncreaseFacade creditCardLimitFacade;
 
-    @GetMapping
-    public ResponseEntity<Boolean> increaseCreditCardLimit(){
-        creditCardLimitFacade.increaseCreditCardLimit(10000);
+    @GetMapping("/{expectedLimit}")
+    public ResponseEntity<Boolean> increaseCreditCardLimit(@PathVariable Integer expectedLimit){
+        //creditCardLimitFacade.increaseCreditCardLimit(10000);
+        creditCardLimitFacade.increaseCreditCardLimit(expectedLimit);
         return new ResponseEntity<>(true, HttpStatus.CREATED);
     }
 }
